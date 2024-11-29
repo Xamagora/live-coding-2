b180639972d862156c1457da89ea546bb9c72a62
Xamagora <xaxiry_magaly@hotmail.com>
2024-11-29 14:49:05 -0600

    Haciendo livecoding

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,6 +1,30 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
+async  function fetchAnswer (){
+
+    try {
+        const response = await fetch(API_ENDPOINT); //se linkea la Api
+        const data = await response.json(); //Trabajar los datos de ls data
+        mostrarRespuesta((data.answer));
+        console.log(data);
+    }catch(err){
+        console.log(err); 
+    }
+
+}
+function mostrarRespuesta(answer){
+    const divAnswer = document.getElementById("answer"); //Se crea una constante para la división con id "answer"
+    divAnswer.textContent = answer; //Para establecer contenido de texto
+    //Limpia la pregunta y la respuesta despues de 5 segundos.
+    //Para ello utilizamos setTimeOut
+    setTimeout(()=>{
+        answerDiv.textContent=" "; // " " para limpiar campos
+        document.getElementById("input").value= " "; // " "Para limpiar
+    }, 5000 );
+}
+fetchAnswer();//Llamar la función 
+
 /**
  * STEPS:
  *
