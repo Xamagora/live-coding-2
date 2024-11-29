2e67e3bfe2d6475dc858b2e669e701c2efd8e3ec
Xamagora <xaxiry_magaly@hotmail.com>
2024-11-29 15:36:17 -0600

    Agregando comentarios

--- starter-files/script.js
+++ starter-files/script.js
@@ -4,12 +4,13 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
 async  function fetchAnswer (){
 
     try {
+        // const response = await fetch('https://yesno.wtf/api'); durante el live coding lo vincule mal 
         const response = await fetch(API_ENDPOINT); //se linkea la Api
         const data = await response.json(); //Trabajar los datos de ls data
-        mostrarRespuesta((data.answer));
+        mostrarRespuesta((data.answer)); //Durante el live coding se me olvido mandarlo a llamar
         console.log(data);
     }catch(err){
-        console.log(err); 
+        console.log(err); //mensaje de error si no lo encuentra
     }
 
 }
@@ -18,12 +19,12 @@ function mostrarRespuesta(answer){
     divAnswer.textContent = answer; //Para establecer contenido de texto
     //Limpia la pregunta y la respuesta despues de 5 segundos.
     //Para ello utilizamos setTimeOut
-    setTimeout(()=>{
+    setTimeout(()=>{ //no lo pude acompletar durante el live coding
         answerDiv.textContent=" "; // " " para limpiar campos
         document.getElementById("input").value= " "; // " "Para limpiar
-    }, 5000 );
+    }, 5000 );// tiempo
 }
-fetchAnswer();//Llamar la función 
+fetchAnswer();//Llamar la función (durante el live coding no lo había mandado a llamar )
 
 /**
  * STEPS:
